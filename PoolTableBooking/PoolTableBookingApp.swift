84e9efb5f1dfbf9ea46144d5ae0700cce64971a4
shilp-tech <shilppatel3100@gmail.com>
2026-03-22 19:05:02 -0500

    Initial Commit

---
+++ PoolTableBooking/PoolTableBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolTableBookingApp.swift
+//  PoolTableBooking
+//
+//  Created by Shilp Patel on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PoolTableBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
